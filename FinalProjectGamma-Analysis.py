c36cf484509c6ad4d0e41421bc010fa65e538313
Dewi SC <113656747+DewiSC@users.noreply.github.com>
2025-02-26 00:16:46 +0700

    Update FinalProjectGamma-Analysis.py

--- FinalProjectGamma-Analysis.py
+++ FinalProjectGamma-Analysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 # Load Model yang sudah disimpan
-model = pickle.load(open('Model_final.pkl', 'rb'))
+model = pickle.load(open('Dataset/Model_final.pkl', 'rb'))
 
 # Judul Aplikasi
 st.title("Hotel Booking Cancellation Prediction")
